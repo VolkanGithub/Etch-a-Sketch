edbf1c3cce4e57d457d52c08f62789fc209ccfd3
Volkan Özkan <volkanozkan@outlook.com>
2024-05-08 06:43:01 +0300

    some improvements again!

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+let color = 'black'
+
 document.addEventListener('DOMContentLoaded', function () {
-  createBoard(64)
+  createBoard(16);
+  let btn_popup = document.querySelector('#popup')
+  btn_popup.addEventListener('click', function () {
+    let size = getSize();
+    createBoard(size);
+  })
 })
 
 function createBoard(size) {
@@ -18,7 +25,38 @@ function createBoard(size) {
       div.style.backgroundColor = 'black'
     })
     board.appendChild(div) */
-    div.style.backgroundColor = 'yellow'
+    div.addEventListener('mouseover', colorDiv)
     board.insertAdjacentElement('beforeend', div)
   }
+}
+
+function getSize() {
+  let input = prompt('Enter the size of the board')
+  let message = document.querySelector('#message')
+  if (input === "" || input === null || isNaN(input)) {
+
+    message.textContent = 'Please enter a valid number'
+  } else if (input < 1 || input > 100) {
+    message.textContent = 'Please enter a number less than 100 and greater than 1'
+  } else {
+    message.textContent = 'You can start drawing'
+    return input;
+  }
+}
+
+function colorDiv() {
+  if (color == 'random') {
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+  } else {
+    this.style.backgroundColor = 'black'
+  }
+}
+
+function setColor(colorChoise) {
+  color = colorChoise;
+}
+
+function resetBoard() {
+  let divs = document.querySelectorAll('div')
+  divs.forEach((div) => div.style.backgroundColor = 'white')
 }
