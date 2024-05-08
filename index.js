d24344d7e773735fe21ad7d0014c71c864a2b9a8
Volkan Özkan <volkanozkan@outlook.com>
2024-05-08 05:13:56 +0300

    some experiments

---
+++ index.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+  createBoard(64)
+})
+
+function createBoard(size) {
+  let board = document.querySelector('.board')
+
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`
+
+  let numberOfDivs = size * size;
+
+  for (let i = 0; i < numberOfDivs; i++) {
+    let div = document.createElement('div')
+    div.classList.add('square')
+    div.addEventListener('mouseover', function () {
+      div.style.backgroundColor = 'black'
+    })
+    board.appendChild(div)
+  }
+}
